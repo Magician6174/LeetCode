76895b7d96f2b4f4d5d1bd08f2b69d95390a60f9
Honey <himanshushar@iisc.ac.in>
2022-05-13 20:52:09 +0530

    Daily Temperature

--- python/739. Daily Temperatures.py
+++ python/739. Daily Temperatures.py
@@ -1,12 +1,37 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = [0]*len(temperatures)
-        stack = []
-        for i, temp in enumerate(temperatures):
+        
+        n = len(temperatures)
+        hottest = 0
+        answer = [0] * n
+        
+        for curr_day in range(n-1,-1,-1):
+            current_temp = temperatures[curr_day]
+            if current_temp >= hottest:
+                hottest = current_temp
+                continue
             
-            while stack and temperatures[stack[-1]] < temp:
-                idx = stack.pop()
-                answer[idx] = i - idx
-            stack.append(i)
+            days = 1
+            while temperatures[curr_day + days] <= current_temp:
+                
+                days += answer[curr_day + days]
+                
+            answer[curr_day] = days
         
         return answer
+    
+"""------------------------------------SOLUTION - 2-----------------------------"""
+
+
+# class Solution:
+#     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+#         answer = [0]*len(temperatures)
+#         stack = []
+#         for i, temp in enumerate(temperatures):
+            
+#             while stack and temperatures[stack[-1]] < temp:
+#                 idx = stack.pop()
+#                 answer[idx] = i - idx
+#             stack.append(i)
+        
+#         return answer
