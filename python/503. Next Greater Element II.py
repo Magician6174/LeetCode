790d8b915c88e1d2299a19a72b5454d21e4905f9
Honey <himanshushar@iisc.ac.in>
2022-05-14 18:11:41 +0530

    Next greater element 2

--- python/503. Next Greater Element II.py
+++ python/503. Next Greater Element II.py
@@ -7,9 +7,10 @@ class Solution:
                 idx = stack.pop()
                 answer[idx] = current_num
             stack.append(i)
-        for i, current_num in enumerate(nums):
-            while stack and nums[stack[-1]] < current_num:
-                idx = stack.pop()
-                answer[idx] = current_num
-            stack.append(i)   
+        if stack: # If stack is empty no need to further check again
+            for i, current_num in enumerate(nums):
+                while stack and nums[stack[-1]] < current_num:
+                    idx = stack.pop()
+                    answer[idx] = current_num
+                stack.append(i)   
         return answer
