af52b82f2821e6f25617ebb3485d50dab26262a0
Honey <himanshushar@iisc.ac.in>
2022-07-25 15:01:03 +0530

    Find duplicate subtrees

---
+++ python/652. Find Duplicate Subtrees.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        
+        def tuplify(root):
+            if root:
+                tuple = root.val, tuplify(root.left), tuplify(root.right)
+                trees[tuple].append(root)
+                return tuple
+        trees = collections.defaultdict(list)
+        tuplify(root)
+        # print(tuplify(root))
+        # print(trees.values())
+        
+        return [roots[0] for roots in trees.values() if roots[1:]]
