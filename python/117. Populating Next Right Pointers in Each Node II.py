d47b796837f059dfef9bba19f3591273f9488457
Honey <himanshushar@iisc.ac.in>
2022-09-09 19:54:45 +0530

    populate next pointer of BT

---
+++ python/117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if root is None:
+            return
+        
+        queue = [root,None]
+        while queue:
+            current = queue.pop(0)            
+            if current == None:
+                if queue:
+                    queue.append(None)
+            else:
+                current.next = queue[0]
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+        return root
+        
