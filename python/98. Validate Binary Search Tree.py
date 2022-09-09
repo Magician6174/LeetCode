2c21101ccb78164a375c2d43241f362eeecac71a
Honey <himanshushar@iisc.ac.in>
2022-09-09 15:53:57 +0530

    Updated validate BST

--- python/98. Validate Binary Search Tree.py
+++ python/98. Validate Binary Search Tree.py
@@ -7,21 +7,93 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
 
-        output = []
-        output = self.inorder(root,output)
+        """---------------Iterative approach------------------"""
+        if not root:
+            return True
         
-        for i in range(1,len(output)):
-            if output[i-1] >= output[i]:
+        stack = [(root,float('-inf'),float('+inf'))]
+        
+        while stack:
+            root,lower,upper = stack.pop()
+            if not root:
+                continue
+            val = root.val
+            if val <= lower or val >= upper:
                 return False
+            stack.append((root.right,val,upper))
+            stack.append((root.left,lower,val))
+            
         return True
-    
-    def inorder(self,root,output):
         
-        if root is None:
-            return 
-        self.inorder(root.left,output)
-        output.append(root.val)
-        self.inorder(root.right,output)
-        return output
+        """-------------Recursive approach-----------------"""
+#         def check(root,lower,upper):
+#             if root is None:
+#                 return True
+#             val = root.val
+#             if val <= lower or val >= upper:
+#                 return False
+#             if not check(root.left,lower,val):
+#                 return False
+#             if not check(root.right,val,upper):
+#                 return False
+#             return True
+            
+#         lower = float('-inf')
+#         upper = float('+inf')
+        
+#         return check(root,lower,upper)
+        
+        
+        """-----------Approach 1------------------"""
+#         output = []
+#         output = self.inorder(root,output)
+        
+#         for i in range(1,len(output)):
+#             if output[i-1] >= output[i]:
+#                 return False
+#         return True
+    
+#     def inorder(self,root,output):
+        
+#         if root is None:
+#             return 
+#         self.inorder(root.left,output)
+#         output.append(root.val)
+#         self.inorder(root.right,output)
+#         return output
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #         if root.left is None and root.right is None:
+#             return True
+#         if root.left and root.right:
+#             if root.val > root.left.val and root.val < root.right.val:
+#                 return self.isValidBST(root.left) and self.isValidBST(root.right)
+#             else:
+#                 return False
+#         elif root.left:
+#             if root.val > root.left.val:
+#                 return self.isValidBST(root.left)
+#             return False
+#         elif root.right:
+#             if root.val < root.right.val:
+#                 return self.isValidBST(root.right)
+#             return False
         
         
