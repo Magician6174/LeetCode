797ff2e3be550201be567020f66d49be2762828f
Honey <himanshushar@iisc.ac.in>
2022-09-13 18:13:07 +0530

    top k elements using heap

--- python/347. Top K Frequent Elements.py
+++ python/347. Top K Frequent Elements.py
@@ -1,9 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = {}
-        for i in nums:
-            if i not in d.keys():
-                d[i] = 1
-            else:
-                d[i] += 1
-        return sorted(d,key=lambda k: d[k], reverse = True)[:k]
+        
+        
+        from collections import Counter
+        from heapq import nlargest
+        d = Counter(nums)
+        counts = d.items()
+        ans = nlargest(k,counts,key=lambda x: x[1])
+        return [i[0] for i in ans]
+        
+        
+        """-------------------First Try-----------------"""
+        # d = {}
+        # for i in nums:
+        #     if i not in d.keys():
+        #         d[i] = 1
+        #     else:
+        #         d[i] += 1
+        # return sorted(d,key=lambda k: d[k], reverse = True)[:k]
